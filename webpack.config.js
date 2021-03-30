e9f9cd67f5c0803e49c9b4deeb60e7aa52e5af83
worksuper2918 <worksuper2918@gmail.com>
2021-03-30 13:29:42 +0900

    [Add] - 자주 사용하는 loader 추가 연습

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,22 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'] // 순서가 중요하다.
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          publicPath: './dist/',
+          name: '[name].[ext]?[hash]', 
+          limit: 10000, // 10kb
+        }
+      }
+    ]
   }
 }
